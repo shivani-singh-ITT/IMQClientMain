6ce3b5bd751b126869859f4ab4acf65983baee76
Shivani  Singh <shivani.singh@intimetec.com>
2021-03-07 09:52:10 +0530

    client

---
+++ src/test/java/modeltest/ClientTest.java
@@ -0,0 +1,21 @@
+package modeltest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Client;
+
+public class ClientTest {
+
+	@Test
+	public void test() {
+		Client client=new Client();
+		client.setClientName("shivani");
+		client.setId("1");
+		assertEquals(client.getClientName(),"shivani");
+		assertEquals(client.getId(),"1");
+		
+	}
+
+}
